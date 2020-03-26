e113b4e556ab27a3854d11354a6fc5135dde1c59
Thorben Wolkersdorfer <thorbenw@notebooksbilliger.de>
2020-03-26 22:13:03 +0100

    Late-submitted definitions for recent enhancements in generate-adoc

--- lib/generate-adoc.d.ts
+++ lib/generate-adoc.d.ts
@@ -4,9 +4,11 @@ export function GenerateReadme(packagePath?: string, readmeFileName?: string, op
     noPackageJsonUpdate?: boolean;
     dependencyType?: string;
 }): void;
-export function Resolve(includeRootSourcePath: any, EOL: any, ...lines: any[]): string;
+export function Resolve(includeRootSourcePath: string, EOL: string, ...lines: any[]): string;
 export function ResolveIncludes(includeRootSourcePath: string, ...lines: string[]): string[];
+export function ResolveAttributes(...lines: string[]): string[];
 export function GetAttribute(attributeName: string, ...lines: string[]): string;
+export function GetAttributes(...lines: string[]): any;
 export type GenerateReadmeOptions = {
     EOL?: string;
     updateTimestamp?: boolean;
